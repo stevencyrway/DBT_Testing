5945a9d51171f8721d4082d623b2b3df4302e4bd
steven cyrway <stevencyrway@gmail.com>
2022-05-27 22:52:28 -0500

    done.

--- Projects/models/SemanticLayer/DropinViews.sql
+++ Projects/models/SemanticLayer/DropinViews.sql
@@ -1,36 +1,34 @@
 -- 4. Identify teams with a drop in year-over-year video views that are within 45 days of their
 -- renewal date
 with videosviewperyear as (
-    Select teams.teamid                                                                                   as team,
-           min(subscription_term_start_date)                                                              as StartDate,
-           max(subscription_term_end_date)                                                                as MaxEndDate,
+    Select teams.teamid                                                            as team,
+           min(subscription_term_start_date)                                       as StartDate,
+           max(subscription_term_end_date)                                         as MaxEndDate,
            "SubscriptionNumber",
-           date_trunc('year', week)                                                                       as Year,
-           sum(actioncount)                                                                               as videoViews,
+           date_trunc('year', week)                                                as Year,
+           sum(actioncount)                                                        as videoViews,
            Lead(sum(actioncount))
-           OVER (PARTITION by teams.teamid ORDER BY date_trunc('year', week) desc)                        as LastyearViews
-    from {{ref('SubscriptionsProd')}}
-        join {{ref('teams')}} on teams.teamid = "SubscriptionsProd".teamid
-        join {{ref('TeamActions')}} on "TeamActions".teamid = teams.teamid
+           OVER (PARTITION by teams.teamid ORDER BY date_trunc('year', week) desc) as LastyearViews
+    from {{ref('SubscriptionsProd')}} join {{ref('teams')}}
+    on teams.teamid = "SubscriptionsProd".teamid
+        join {{ref ('TeamActions')}} on "TeamActions".teamid = teams.teamid
         and week >= subscription_term_start_date and week <= subscription_term_end_date
     where actiontype = 'video views'
     group by teams.teamid, "SubscriptionNumber", date_trunc('year', week)
-    order by teams.teamid asc, date_trunc('year', week) asc),
+    order by teams.teamid asc, date_trunc('year', week) asc)
 
-     viewsdifference as (Select team,
-                                year,
-                                VideoViews,
-                                LastyearViews,
-                                MaxEndDate,
-                                sum(videoViews) - sum(LastyearViews) as VideoDifference
-                         from videosviewperyear
-                         group by team, year, VideoViews, LastyearViews, MaxEndDate)
+Select team,
+       year,
+       VideoViews,
+       LastyearViews,
+       MaxEndDate,
+       sum(videoViews) - sum(LastyearViews) as VideoDifference
+from videosviewperyear
+group by team, year, VideoViews, LastyearViews, MaxEndDate
+having (sum(videoViews) - sum(LastyearViews)) < 0
 
-Select *
-from viewsdifference
-where VideoDifference < 0
 
--- from {{ref('SubscriptionsProd')}}
---                                 join {{ref('teams')}} on teams.teamid = "SubscriptionsProd".teamid
---                                 join {{ref('TeamActions')}} on "TeamActions".teamid = teams.teamid
---                                 and week >= subscription_term_start_date and week <= subscription_term_end_date
+
+
+
+
